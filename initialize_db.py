683867cab815cf2c89e3101b7788c237286d0a9c
Anatolii <abordovskyi@gmail.com>
2024-08-07 22:13:14 +0300

    Commit 17 "Подключена база данных. Определяется имя личного телеграмма юзера"

--- initialize_db.py
+++ initialize_db.py
@@ -1,37 +1,28 @@
 import sqlite3
 
-def create_connection(db_file):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except sqlite3.Error as e:
-        print(e)
-    return conn
-
-def initialize_database():
-    conn = create_connection('user_sessions.db')
+def initialize_db():
+    conn = sqlite3.connect('user_sessions.db')
     cursor = conn.cursor()
 
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS user_sessions (
-        session_id INTEGER PRIMARY KEY,
-        user_id INTEGER,
-        language TEXT,
+        user_id INTEGER PRIMARY KEY,
+        username TEXT,
         user_name TEXT,
-        event_date TEXT,
+        language TEXT,
+        selected_date TEXT,
         start_time TEXT,
         end_time TEXT,
-        number_of_people INTEGER,
-        party_style TEXT,
-        preferences TEXT,
+        duration INTEGER,
+        people_count INTEGER,
+        selected_style TEXT,
         city TEXT,
-        username TEXT -- добавляем новое поле для username
+        preferences TEXT
     )
     ''')
 
     conn.commit()
     conn.close()
 
-if __name__ == "__main__":
-    initialize_database()
+if __name__ == '__main__':
+    initialize_db()
